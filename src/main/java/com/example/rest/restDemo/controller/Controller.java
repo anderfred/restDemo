ff17fd283116e79b039b3a7999ac2bd6ee2562f0
Egor Belkov <Subgorn@gmail.com>
2018-11-05 18:54:36 +0700

    add List<Item> REST

--- src/main/java/com/example/rest/restDemo/controller/Controller.java
+++ src/main/java/com/example/rest/restDemo/controller/Controller.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class Controller {
     private Logger logger = LoggerFactory.getLogger(Controller.class);
@@ -17,13 +20,20 @@ public class Controller {
     }
 
     @RequestMapping("/item")
-    public Item index() {
+    public Item item() {
         Item item = itemService.getByName("fredx").get();
         if (item != null) {
             logger.info("/item REST {}", item);
             return item;
         }
         return null;
+    }
+
+    @RequestMapping("/items")
+    public List<Item> items() {
+        List<Item> list = new ArrayList<>();
+        itemService.findAll().forEach(item -> list.add(item));
+        return list;
     }
 
 }
